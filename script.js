61a11658feb79c0655514152350c975fbff83442
Othei26 <gutierrezmaiker@gmail.com>
2023-12-16 03:03:53 -0600

    Add remaining Features
    Added the Draw, Eraser, Clear and Rainbow Mode Features.

--- script.js
+++ script.js
@@ -1,133 +1,154 @@
-let gridSize = document.querySelector("#grid_size").value;
+const gridSizeInput = document.querySelector("#grid_size");
 const container = document.querySelector(".container");
-const cellsCurrentColor = document.querySelector("#cells_color_picker");
-const gridCurrentColor = document.querySelector("#grid_color_picker");
+const cellsColorPicker = document.querySelector("#cells_color_picker");
+const gridColorPicker = document.querySelector("#grid_color_picker");
+const eraserBtn = document.querySelector("#eraserBtn");
+const drawBtn = document.querySelector("#drawBtn");
+const clearBtn = document.querySelector("#clearBtn");
+const rainbowBtn = document.querySelector("#rainbowBtn");
+const btnOption = document.querySelector("#btn_option");
 
 
 // Function to create the Grid
 
 function createGrid() {
-    for (let i = 0; i < gridSize; i++) {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < gridSizeInput.value; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
-        for (let j = 0; j < gridSize; j++) {
+
+        for (let j = 0; j < gridSizeInput.value; j++) {
             const cell = document.createElement("div");
             cell.classList.add("cells");
             row.appendChild(cell);
         }
-        container.appendChild(row);
-    }
-}
 
+        fragment.appendChild(row);
+    }
+
+    container.textContent = "";
+    container.appendChild(fragment);
+}
 
 // Function to change the Grid Size depending on the User Selection
 
 function changeGridSize() {
-    const rangeSelector = document.querySelector("#grid_size");
-    
-    rangeSelector.addEventListener("change", () => {
-        gridSize = document.querySelector("#grid_size").value;
-        addCurrentGridSizeValue()
-        resetGridSize();
+
+    gridSizeInput.addEventListener("change", () => {
+        createGrid()
+        addCurrentGridSizeValue();
         clearCells();
         changeGridColor();
     });
 }
 
-
 // Function to modify the Current Grid Size Value in the UI
 
 function addCurrentGridSizeValue() {
-    const grid_size_toggle = document.querySelector("#current_value");
-    
-    grid_size_toggle.textContent = "";
-    grid_size_toggle.textContent = ` ${gridSize} x ${gridSize}`;
+    const gridSizeToggle = document.querySelector("#grid_size_current_value");
+    gridSizeToggle.textContent = ` ${gridSizeInput.value} x ${gridSizeInput.value}`;
 }
 
-
-// Function to Reset the current Grid Size
-function resetGridSize() {
-    container.textContent = "";
-    createGrid();
-}
-
-
-// Function to Apply the new color in the Grid
-
-function applyColor() {
-    container.addEventListener ("click", () => {
-        container.addEventListener ("mouseover", (event) => {
-            if (event.target.classList.contains("cells")) {
-                changeCellColor(event.target)
-            };
-        })
-    })
-}
-
-
-// Function to change the color of the cells
-
-function changeCellColor(event) {
-    let cellColorSelected = cellsCurrentColor.value;
-
-    event.style.backgroundColor = cellColorSelected;
-}
-
-
-// Function to change the color of the cells
+// Function to change the color of the Grid
 
 function changeGridColor() {
-    const gridy = document.querySelectorAll(".cells");
+    const cells = document.querySelectorAll(".cells");
     
-    gridy.forEach((grid) => {
-        testy(grid)
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = gridColorPicker.value;
     })
 }
 
-
-// // Function to change the color of the Grid
-
-// function testy(value) {
-
-//     let gridColorSelected = gridCurrentColor.value;
-
-//     if (value.classList.contains("cells")) {
-//         value.removeAttribute("style")
-//         value.style.backgroundColor = gridColorSelected;
-//     }
-// }
+// Event listener for Grid color input change
+gridColorPicker.addEventListener("input", changeGridColor);
 
 
-// gridCurrentColor.addEventListener("input", () => {
-//     changeGridColor();
-// });
+// Function to select All the Cells in the Grid and Clear them.
 
+function clearCells() {
+    const cells = document.querySelectorAll(".cells");
 
-// // Function to select all the cells in the Grid and clear them.
+    cells.forEach((cell) => {
+        cell.removeAttribute("style");
+    });
+}
 
-// function clearCells() {
-//     const cells = document.querySelectorAll(".cells");
+// Event listener for Clear Button click
+clearBtn.addEventListener("click", (event) => {
+    applyButtonColorEffect(event.target);
+    clearBtnColorEffect()
+    clearCells();
+    changeGridColor();
+});
 
-//     cells.forEach((cell) => {
-//         clearGridCells(cell);
-//     });
-// }
+// Function to put a Color effect to the Clear Button
+function clearBtnColorEffect() {
+    setTimeout(() => {
+        clearBtn.removeAttribute("style");
+    }, 120);
+}
 
+// Event listener for Eraser Button Click
+eraserBtn.addEventListener("click", (event) => {
+    clearPreviousBtnColorEffect(drawBtn)
+    clearPreviousBtnColorEffect(rainbowBtn)
+    applyButtonColorEffect(event.target);
 
-// // Function to clear the Grid
-// function clearGridCells(value) {
-//     const clear = document.querySelector("#clear");
+    container.addEventListener ("mouseover", (cell) => {
+        eraseColor(cell.target);
+    })
+})
 
-//     clear.addEventListener("click", () => {
-//         if (value.classList.contains("cells")) {
-//             changeGridColor();
-//         }
-//     });
-// }
+// Function to erase the Cell Color when using the Eraser Button
+function eraseColor(cell) {
+    cell.style.backgroundColor = gridColorPicker.value;
+}
 
+// Event listener for Draw Button Click
+drawBtn.addEventListener("click", (event) => {
+    clearPreviousBtnColorEffect(eraserBtn)
+    clearPreviousBtnColorEffect(rainbowBtn)
+    applyButtonColorEffect(event.target);
 
+    container.addEventListener ("mouseover", (cell) => {
+        applyColor(cell.target);
+    })
+})
 
-// createGrid();
-// changeGridSize();
-// applyColor();
-// clearCells()
+// Function to apply the Cell Color when using the Draw Button
+function applyColor(cell) {
+    cell.style.backgroundColor = cellsColorPicker.value;
+}
+
+// Function to apply a Color to a Button.
+function applyButtonColorEffect(color) {
+    color.style.backgroundColor = "#0099ff";
+    color.style.color = "white";
+}
+
+// Function to Clear the Previus Button Color Effect
+function clearPreviousBtnColorEffect(btn) {
+    btn.removeAttribute("style");
+}
+
+// Event listener for Rainbow Button Click
+rainbowBtn.addEventListener("click", (event) => {
+    clearPreviousBtnColorEffect(drawBtn)
+    clearPreviousBtnColorEffect(eraserBtn)
+    applyButtonColorEffect(event.target);
+
+    container.addEventListener ("mouseover", (event) => {
+        randomColor(event.target);
+    })
+})
+
+// Function to generate a Random Color
+function randomColor(event) {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    event.style.backgroundColor = randomColor;
+}
+
+// Create Grid in the First Webpage Load
+createGrid();
+changeGridSize();
